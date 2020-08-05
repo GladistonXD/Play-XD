141fd25e60ba82a4c89ba033be2e8268c85cf03d
GladistonXD <50533550+GladistonXD@users.noreply.github.com>
2020-08-05 20:47:24 -0300

    Update default.py

--- default.py
+++ default.py
@@ -8,7 +8,7 @@ import codecs
 from six.moves.html_parser import HTMLParser
 #import urlresolver
 #from bs4 import BeautifulSoup
-Versao = "20.56.00"
+Versao = "20.57.00"
 
 AddonID = 'plugin.video.GladistonXD'
 Addon = xbmcaddon.Addon(AddonID)
@@ -166,7 +166,7 @@ def MFilmes(): #-2
 	AddDir("[COLOR blue][B]Filmes Legendado RedeCanais[/B][/COLOR]" , cPageleg, 91, "https://walter.trakt.tv/images/movies/000/181/313/fanarts/thumb/cc9226edfe.jpg", "https://walter.trakt.tv/images/movies/000/181/313/fanarts/thumb/cc9226edfe.jpg", background="cPageleg", info='[COLOR][/COLOR]')
 	AddDir("[COLOR blue][B]Filmes Nacional RedeCanais[/B][/COLOR]" , cPagenac, 92, "http://cdn.cinepop.com.br/2016/11/minhamaeeumapeca2_2-750x380.jpg", "http://cdn.cinepop.com.br/2016/11/minhamaeeumapeca2_2-750x380.jpg", background="cPagenac", info='[COLOR][/COLOR]')
 	#AddDir("[COLOR purple][B]Filmes FilmesOnline[/B][/COLOR]" , "", 170, "https://uploaddeimagens.com.br/images/002/428/080/original/ROBOZIm.jpg", "https://uploaddeimagens.com.br/images/002/428/080/original/ROBOZIm.jpg")
-	AddDir("[COLOR lightgreen][B]Filmes Superflix[/B][/COLOR]" , "", 411, "https://uploaddeimagens.com.br/images/002/428/080/original/ROBOZIm.jpg", "https://uploaddeimagens.com.br/images/002/428/080/original/ROBOZIm.jpg")
+	AddDir("[COLOR lightgreen][B]Filmes Superflix[/B][/COLOR]" , "", 411, "https://uploaddeimagens.com.br/images/002/428/080/original/ROBOZIm.jpg", "https://uploaddeimagens.com.br/images/002/428/080/original/ROBOZIm.jpg", info='[COLOR][/COLOR]')
 	setViewM()
 def MSeries(): #-3
 	AddDir("[COLOR yellow][B]Séries NetCine[/B][/COLOR]" , "", 60, "https://walter.trakt.tv/images/shows/000/098/898/fanarts/thumb/bca6f8bc3c.jpg", "https://walter.trakt.tv/images/shows/000/098/898/fanarts/thumb/bca6f8bc3c.jpg")
@@ -174,7 +174,7 @@ def MSeries(): #-3
 	AddDir("[COLOR blue][B]Animes RedeCanais[/B][/COLOR]" , cPageser, 140, "https://walter.trakt.tv/images/shows/000/098/580/fanarts/thumb/d48b65c8a1.jpg", "https://walter.trakt.tv/images/shows/000/098/580/fanarts/thumb/d48b65c8a1.jpg", background="cPageser", info='[COLOR][/COLOR]')
 	AddDir("[COLOR blue][B]Desenhos RedeCanais[/B][/COLOR]" , cPageani, 150, "https://walter.trakt.tv/images/shows/000/069/829/fanarts/thumb/f0d18d4e1d.jpg", "https://walter.trakt.tv/images/shows/000/069/829/fanarts/thumb/f0d18d4e1d.jpg", background="cPageser", info='[COLOR][/COLOR]')
 	AddDir("[B][COLOR cyan]Séries MMFilmes[/COLOR][/B]", "config" , 190,"https://walter.trakt.tv/images/shows/000/037/522/fanarts/thumb/6ecdb75c1c.jpg", "https://walter.trakt.tv/images/shows/000/037/522/fanarts/thumb/6ecdb75c1c.jpg", isFolder=True, info='[COLOR][/COLOR]')
-	AddDir("[B][COLOR lightgreen]Séries Superflix[/COLOR][/B]", "config" , 401,"https://walter.trakt.tv/images/shows/000/037/522/fanarts/thumb/6ecdb75c1c.jpg", "https://walter.trakt.tv/images/shows/000/037/522/fanarts/thumb/6ecdb75c1c.jpg", isFolder=True)
+	AddDir("[B][COLOR lightgreen]Séries Superflix[/COLOR][/B]", "config" , 401,"https://walter.trakt.tv/images/shows/000/037/522/fanarts/thumb/6ecdb75c1c.jpg", "https://walter.trakt.tv/images/shows/000/037/522/fanarts/thumb/6ecdb75c1c.jpg", isFolder=True, info='[COLOR][/COLOR]')
 	AddDir("[B][COLOR springgreen]Séries QueroFilmesHD[/COLOR][/B]", "config" , 430,"https://cdn.mensagenscomamor.com/content/images/p000024904.jpg?v=2", "https://cdn.mensagenscomamor.com/content/images/p000024904.jpg?v=2", isFolder=True)
 	#AddDir("[B][COLOR mediumpurple]Séries Vizer.tv[/COLOR][/B]", "config" , 450,"https://cdn.mensagenscomamor.com/content/images/p000024904.jpg?v=2", "https://cdn.mensagenscomamor.com/content/images/p000024904.jpg?v=2", isFolder=True)
 	setViewM()
@@ -833,19 +833,19 @@ def QuerofilmeshdLista(): #511
     try:	
 		link2 = requests.get(url)
 		link = common.OpenURL(url).replace('\n','').replace('\r','')
-		sinopse = re.compile('Sinopse.+?<p>(.+?)<\/p>').findall(link)
+		sinopse = re.compile('wp-content">.+?<p>(.+?)<\/p>').findall(link)
 		sinopse= sinopse[0].replace(' &#8211; Online Dublado e Legendado','').replace('Assistir ','').replace('&#8217;','’').replace('&#8211;','–').replace('&#038;','&').replace('&#8216;','‘').replace('&#8220;','“').replace('&#8221;','”').replace('&#8230;','…')
 		match2 = re.findall("online\/.p=([^\"]+)'", link2.text)
 		url3 = ('https://querofilmeshd.online/wp-admin/admin-ajax.php')
 		headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 		result = {'action': 'doo_player_ajax', 'post': match2, 'nume': '1','type': 'movie'}
 		f = requests.post(url3, data = result, headers=headers)
-		url4 = re.compile("(https[^\"]+)' frame").findall(f.text)
-		url4 = url4[0]
+		url4 = re.compile("(https[^\"]+)").findall(f.text)
+		url4 = url4[0].replace("\/\/","//").replace("\/","/")
 		link3 = requests.get(url4)
 		w2 = link3.text
 		match4 = re.findall('idS:."([^\"]+)', w2)
-		name5 = re.compile('Server.#..(\w+)').findall(w2)
+		name5 = re.compile('">\w+.#..(\w+)').findall(w2)
 		if match4:
 			for url2 in match4:
 				arquivo = open(cachefolder + "querofilmeshex", "w+")
@@ -944,8 +944,8 @@ def PlayEpiQF(): #433
 		headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 		result = {'action': 'doo_player_ajax', 'post': match2, 'nume': '1', 'type': 'tv'}
 		f = requests.post(url3, data=result, headers=headers)
-		url4 = re.compile("(https[^\"]+)' frame").findall(f.text)
-		url4 = url4[0].replace("[u'https", "https")
+		url4 = re.compile("(https[^\"]+)").findall(f.text)
+		url4 = url4[0].replace("[u'https", "https").replace("\/\/","//").replace("\/","/")
 		link3 = requests.get(url4)
 		w2 = link3.text
 		match3 = re.findall('idS."([^\"]+)', w2)
