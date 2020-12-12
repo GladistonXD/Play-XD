04654b805ca8e125473276125a8ee28dc778b5ea
GladistonXD <50533550+GladistonXD@users.noreply.github.com>
2020-12-12 06:51:44 -0300

    Update default.py

--- default.py
+++ default.py
@@ -9,7 +9,7 @@ import codecs
 from six.moves.html_parser import HTMLParser
 #import urlresolver
 #from bs4 import BeautifulSoup
-Versao = "21.08.00"
+Versao = "21.09.00"
 
 AddonID = 'plugin.video.GladistonXD'
 Addon = xbmcaddon.Addon(AddonID)
@@ -2724,7 +2724,7 @@ def TVCB4PLAY(): #109
 				program = re.compile('"ongoing">.+?class="(time">.+?)<.+?\/>(.+?<\/h2).+?title">(.+?)<.+?progress-(container">.+?<div).+?(:.+?)"').findall(url4[0])
 				program2 = re.compile('lass="(time">.+?)<.+?\/>(.+?<\/h2).+?title">(.+?<\/span)').findall(url4x[0])
 				ir1 = str(program).replace('<div','').replace("')]","").replace("('","").replace('"), ',"").replace("'), ","").replace("', '"," - ").replace("[","").replace("', ","").replace('"',' - ').replace('time - >','[COLOR red]AO VIVO:[/COLOR] ').replace("</h2"," | Gênero/EP").replace(" <span class='rating'>"," IMDB: ").replace("<span class='rating good'>"," IMDB: ").replace("container - >","Tempo:").replace('</span>','').replace(' - Tempo:','\n[COLOR yellow]TEMPO:[/COLOR] ').replace('- :',' : ').replace('%',"% concluído\n\n").replace("'Tempo:",'\n[COLOR yellow]TEMPO:[/COLOR] ')
-				ir2 = str(program2).replace('</span','\n\n').replace("')]","").replace("('","").replace('"), ',"").replace("'), ","").replace("', '"," - ").replace("[","").replace("', ","").replace('"',' - ').replace('time - >','Horário: ').replace("</h2"," | Gênero/EP").replace(" <span class='rating'>"," IMDB: ").replace("<span class='rating good'>"," IMDB: ").replace('><p class= - title - >','').replace(']','').replace('- )','').replace("-,'",'')
+				ir2 = str(program2).replace('</span','\n\n').replace("')]","").replace("('","").replace('"), ',"").replace("'), ","").replace("', '"," - ").replace("[","").replace("', ","").replace('"',' - ').replace('time - >','Horário: ').replace("</h2"," | Gênero/EP").replace(" <span class='rating'>"," IMDB: ").replace("<span class='rating good'>"," IMDB: ").replace('><p class= - title - >','').replace(']','').replace('- )','').replace("-,",'')
 				title = " - "+title[0]
 				url5 = "[B]"+'\n\n\n'+ir1+"[/B]" + ir2
 				url6 = url5.decode('string_escape')
